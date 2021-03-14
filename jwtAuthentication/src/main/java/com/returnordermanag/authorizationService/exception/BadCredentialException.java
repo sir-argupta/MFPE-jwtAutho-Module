b7fb290457c246ba552f6c394da223c7b0c4f2af
Ashutosh Gupta <aashutosh.g27@gmail.com>
2021-03-15 05:20:44 +0530

    Delete jwtAuthentication directory

--- jwtAuthentication/src/main/java/com/returnordermanag/authorizationService/exception/BadCredentialException.java
+++
@@ -1,9 +0,0 @@
-package com.returnordermanag.authorizationService.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code=HttpStatus.NOT_FOUND , reason="Username/Password incorrect")
-public class BadCredentialException extends Exception {
-
-}
